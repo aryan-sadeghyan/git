257bbd211e52de22c0c9e141e924f79a6c41bd20
aryan <aryansadeghyan79@gmail.com>
2023-10-02 19:53:34 +0330

    version 3

--- change.js
+++ change.js
@@ -1,7 +1,7 @@
 console.log("change");
-console.log("version 2");
-console.log("version 2");
-console.log("version 2");
-console.log("version 2");
-console.log("version 2");
-console.log("version 2");
+console.log("version 3");
+console.log("version 3");
+console.log("version 3");
+console.log("version 3");
+console.log("version 3");
+console.log("version 3");
